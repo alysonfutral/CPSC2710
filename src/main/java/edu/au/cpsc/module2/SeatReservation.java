f4b06a6998f10f498328e5ae5951f90fec6533b1
Alyson Futral <alysonlfutral@gmail.com>
2024-08-30 14:23:41 -0700

    module2 commit

---
+++ src/main/java/edu/au/cpsc/module2/SeatReservation.java
@@ -0,0 +1,69 @@
+import java.time.LocalDate;
+
+public class SeatReservation {
+    // instance variables
+    private String flightDesignator;
+    private java.time.LocalDate flightDate;
+    private String firstName;
+    private String lastName;
+
+    public String getFlightDesignator() {
+        return flightDesignator;
+    }
+
+    public void setFlightDesignator(String fd) {
+        //this.flightDesignator = fd;
+        
+        if (fd == null){
+            throw new IllegalArgumentException("flight designator cannot be null");
+        }
+
+        if (fd.length() < 4 || fd.length() > 6) {
+            throw new IllegalArgumentException("The flightDesignator's length should be between 4 and 6 characters.");
+        }
+
+        this.flightDesignator = fd;
+    }
+
+    // return using .toString()
+    public LocalDate getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(LocalDate date) {
+        this.flightDate = date;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fn) {
+        this.firstName = fn;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String ln) {
+        this.lastName = ln;
+    }
+
+    public String toString() {
+        /*return a string representing this object using 
+        *"null" for all instance variables that are null.
+        *
+        * ternary operator for null checks
+        */
+
+        return "SeatReservation{flightDesignator=" + 
+           (flightDesignator == null ? "null" : flightDesignator) +
+           ",flightDate=" + (flightDate == null ? "null" : flightDate.toString()) +
+           ",firstName=" + (firstName == null ? "null" : firstName) +
+           ",lastName=" + (lastName == null ? "null" : lastName) + 
+           "}";
+
+    }
+
+}
